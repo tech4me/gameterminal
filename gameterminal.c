db0856b25c49bb9e1b930ed8b7be24a228a95a14
Steven Yin <guiwanglong@gmail.com>
2015-11-04 18:56:34 -0500

    fix

--- gameterminal.c
+++ gameterminal.c
@@ -1,8 +1,10 @@
-/* 
+/*
  * File:   main.c
  * Author: tech4me
+ * Description: A program that takes two board game programs and execicute them
+ * find the winner.(Have program "A" and "B" under the same directory as this
+ * program.)
  *
- * 
  */
 
 #include <stdio.h>
@@ -14,25 +16,26 @@
 #include <sys/types.h>
 #include <unistd.h>
 
+int pfork(int x);
 bool checkFile();
 void runPrograms();
 
-int main(int argc, char* argv[])//"boardsize" "BW or WB" BW= AB BW | WB= AW BB 
+int main(int argc, char* argv[])//"boardsize" "BW or WB" BW= AB BW | WB= AW BB
 {
     int boardSize;
     char* endPtr;
     boardSize = strtoimax(argv[1], &endPtr, 10);
     char colourA, colourB;
     colourA = *argv[2];
-    colourB = *(argv[2] + 1);
-    
-    if (checkFile())
+    colourB = *( argv[2] + 1 );
+
+    if ( checkFile())
     {
         runPrograms();
     }
     else
         printf("Not able to find executables 'A' and 'B',exiting!\n");
-    return (EXIT_SUCCESS);
+    return ( EXIT_SUCCESS );
 }
 
 bool checkFile(void)
@@ -41,20 +44,20 @@ bool checkFile(void)
     DIR *d;
     struct dirent *dir;
     d = opendir(".");
-    if (d)
+    if ( d )
     {
-        while ((dir = readdir(d)) != NULL)
+        while (( dir = readdir(d)) != NULL )
         {
-            if (dir->d_type == DT_REG)
+            if ( dir->d_type == DT_REG )
             {
-                if ((*(dir->d_name) == 'A' || *(dir->d_name) == 'B')&&(*(dir->d_name+1)!='.'))
+                if (( *( dir->d_name ) == 'A' || *( dir->d_name ) == 'B' ) && ( *( dir->d_name + 1 ) != '.' ))
                     counter++;
                 //printf("%s\n", dir->d_name);
             }
         }
         closedir(d);
     }
-    if (counter == 2)
+    if ( counter == 2 )
         return true;
     else
         return false;
@@ -65,127 +68,111 @@ void runPrograms()
     char a;
     pid_t pid;
     int rv;
-    int commpipe1[2];// This holds the fd for the input & output of the pipe  Setup communication pipeline first 
-    if (pipe(commpipe1))
-    {
-        fprintf(stderr, "Pipe error!\n");
-        exit(1);
-    }
-    if ((pid = fork()) == -1)// Attempt to fork and check for errors
-    {
-        fprintf(stderr, "Fork error. Exiting.\n");
-        exit(1); // something went wrong
-    }
-    if (pid)// A positive (non-negative) PID indicates the parent process
-    {
-        dup2(commpipe1[1], 1); // Replace stdout with out side of the pipe
-        close(commpipe1[0]); // Close unused side of pipe (in side)
-        setvbuf(stdout, (char*) NULL, _IONBF, 0);  // Set non-buffered output on stdout
-        fprintf(stdout,"ab");
-        wait(&rv);
-    }
-    else// A zero PID indicates that this is the child process 
-    {
-        dup2(commpipe1[0], 0); // Replace stdin with the in side of the pipe
-        close(commpipe1[1]); // Close unused side of pipe (out side) Replace the child fork with a new process
-        {
-        if (execl("A", "A", NULL) == -1)
-            fprintf(stderr, "execl Error!");
-            exit(1);
-        }
-    }
-}
-
-int pfork(int);
-
-int main(void) {
-    int parentA[2];// This holds the fd for the input & output of the pipe  Setup communication pipeline first 
+    int parentA[2];    // The four pipes
     int childA[2];
-    
+
     int parentB[2];
     int childB[2];
-    
-    char buff[50];
-    
-    if (pipe(parentA) || pipe(childB) || pipe(parentA) ||pipe(childB) )
+
+    char buffA[50];
+    char buffB[50];
+
+
+    if ( pipe(parentA) || pipe(childA) || pipe(parentB) || pipe(childB) )
     {
         fprintf(stderr, "Pipe error!\n");
         exit(1);
     }
-    
+
     int i;
     i = pfork(3);
-    
-    if (i == 0)// A positive (non-negative) PID indicates the parent process    // parent
+
+    if ( i == 0 )    // A positive (non-negative) PID indicates the parent process    // parent
     {
         const char love[] = "love your sons!\n";
-        
+        setvbuf(stdout, (char*) NULL, _IONBF, 0);
         int inA, outA;
         inA = parentA[0];
         outA = childA[1];
-        
+        close(parentA[1]);
+        close(childB[0]);
+
         int inB, outB;
         inB = parentB[0];
         outB = childB[1];
-        
-        
-        
-        read (inA, buff, 50);
-        write (outB, buff,strlen(buff) + 1);
-        
+        close(parentB[1]);
+        close(childB[0]);
+
+        sleep(1);
+        write(outA, "abc", 4);
+        write(outB, "def", 4);
+        read (inA, buffA, 50);
+        read (inB, buffB, 50);
+        printf("%s%s\n", buffA, buffB);
+        //write (outB, buff,strlen(buff) + 1);
+
         /*dup2(commpipeAParent[0], 0); // Replace stdin with the in side of the pipe
-        close(commpipeAParent[1]); // Close unused side of pipe (out side) Replace the child fork with a new process
-        {
-        if (execl("A", "A", NULL) == -1)
-            fprintf(stderr, "execl Error!");
-            exit(1);
-        }*/
-    } else if (i==1)    // child A
-     // A zero PID indicates that this is the child process 
+           close(commpipeAParent[1]); // Close unused side of pipe (out side) Replace the child fork with a new process
+           {
+           if (execl("A", "A", NULL) == -1)
+           fprintf(stderr, "execl Error!");
+           exit(1);
+           }*/
+    }
+    else if ( i == 1 )            // child A
+    // A zero PID indicates that this is the child process
     {
-        char const hi[] = "hi\n";
-        
+        setvbuf(stdout, (char*) NULL, _IONBF, 0);
         int in, out;
         in = childA[0];
         out = parentA[1];
-        
-      
-        write (out,hi,strlen(hi)+1);
+        close(childA[1]);
+        close(parentA[0]);
+
+        dup2(in, 0);
+        dup2(out, 1);
+        //write (out,hi,strlen(hi)+1);
         //read(in, buff, 50);
-        
-       /* dup2(commpipeAParent[1], 1); // Replace stdout with out side of the pipe
-        close(commpipeAParent[0]); // Close unused side of pipe (in side)
-        setvbuf(stdout, (char*) NULL, _IONBF, 0);  // Set non-buffered output on stdout
-        fprintf(stdout,);
-        wait(&rv);
-    */
-    } else if (i==2) {  // child B
-        char const hi[] = "Not\n";
-        
+        char temp[50];
+        //scanf("%s",temp);
+        printf("%s", temp);
+
+        /* dup2(commpipeAParent[1], 1); // Replace stdout with out side of the pipe
+           close(commpipeAParent[0]); // Close unused side of pipe (in side)
+           setvbuf(stdout, (char*) NULL, _IONBF, 0);  // Set non-buffered output on stdout
+         */
+    }
+    else if ( i == 2 )              // child B
+    {
+        setvbuf(stdout, (char*) NULL, _IONBF, 0);
         int in, out;
         in = childB[0];
         out = parentB[1];
-        
-        read(in, buff, 50);
-       // write(out, hi, strlen(hi)+1)
-        printf("%s",buff);
-        
+        close(childB[1]);
+        close(parentB[0]);
+        dup2(in, 0);
+        dup2(out, 1);
+        //read(in, buff, 50);
+        // write(out, hi, strlen(hi)+1)
+        char temp[50];
+        //scanf("%s",temp);
+        printf("%s", temp);
+
     }
-    
-    
-    return 0;
+
+
 }
 
 int pfork(int x)
 {
-        int j;
-        for(j=1;j<x;j++)
+    int j;
+    for( j = 1; j < x; j++ )
+    {
+        if( fork() == 0 )
         {
-                if(fork()==0)
-                {
-                    //sleep(3);
-                    return j;
-                }
+            //sleep(3);
+            return j;
         }
-        return 0;
+    }
+    return 0;
 }
